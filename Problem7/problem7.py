e7222a50b71a94442746fec86d70fa64cac97b15
colsonmarco <colsonmarco@gmail.com>
2022-04-04 17:52:28 -0400

    Finish Problem 7

---
+++ Problem7/problem7.py
@@ -0,0 +1,7 @@
+# Functions from prime_kit.py are in the Helpers/prime_kit/__init__.py file in
+# the repo.
+
+from prime_kit import sieve_of_eratosthenes as sieve
+
+primes = sieve(105000)
+print(primes[10000])
