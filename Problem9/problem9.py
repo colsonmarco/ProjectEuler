598eb50e88eda42b110b974f86df553b0f5b599e
colsonmarco <colsonmarco@gmail.com>
2022-04-05 17:13:07 -0400

    Finish Problem 9

---
+++ Problem9/problem9.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+# Question can be found at https://projecteuler.net/thread=9
+
+for a in range(1001):
+    for b in range(a, 1001):
+        c = sqrt(a ** 2 + b ** 2)
+        if c % 1 == 0 and a + b + c == 1000 and (a != b and c > a and c > b):
+            print(a * b * c)
+            break
+
